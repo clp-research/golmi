5db7432f1ed0664a31fe3cbce57aeab38d3dcdb1
seba <seba.g90@gmail.com>
2022-01-09 12:32:08 +0100

    init unit tests for model logic

---
+++ tests/test_model.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import unittest
+
+from model.model import Model
+from model.config import Config
+
+
+class FakeSocket:
+    """
+    we are not testing socket connection here
+    and do not need a real socket
+    """
+    def emit(self, *args, **kwargs):
+        pass
+
+
+class Test(unittest.TestCase):
+    """
+    tests on model
+    """
+    config = Path(
+        "app/pentomino/static/resources/config/pentomino_config.json"
+    )
+
+    def get_model(self):
+        return Model(
+            Config.from_json(self.config),
+            FakeSocket(),
+            "TestRoom"
+        )
+
+    def test_add_gripper(self):
+        model = self.get_model()
+        model.add_gr(100)
+
+        # model should have one gripper
+        self.assertEqual(len(model.state.grippers), 1)
+
+    def test_remove_gripper(self):
+        model = self.get_model()
+        model.add_gr(100)
+
+        model.remove_gr(100)
+        self.assertEqual(len(model.state.grippers), 0)
+
+    def test_start_gripping(self):
+        model = self.get_model()
+        model.add_gr(100)
+
+        model.start_gripping(100)
+
+        # a gripping loop should be present in the
+        # running_loops dictionary of the model
+        # and has a value which is not None
+        self.assertTrue(
+            model.running_loops["grip"][100] is not None
+        )
+
+    def test_stop_gripping(self):
+        model = self.get_model()
+        model.add_gr(100)
+
+        model.start_gripping(100)
+        model.stop_gripping(100)
+
+        # a gripping loop should be present in the
+        # running_loops dictionary of the model
+        # and has a value which is None
+        self.assertTrue(
+            model.running_loops["grip"][100] is None
+        )
