bc3edd87c3727453bc2ecd2825f805d7110828ef
seba <seba.g90@gmail.com>
2022-03-16 17:43:02 +0100

    init descrimage (copy from pentomino)

--- app/register_experiments.py
+++ app/register_experiments.py
@@ -1,7 +1,7 @@
 from app import REGISTRY, Experiment
 from app.welcome import welcome
 from app.pentomino import pentomino
-
+from app.descrimage import descrimage
 
 def register_app(app):
     # The welcome page (will automatically list
@@ -17,14 +17,14 @@ def register_app(app):
         )
     )
     # Your custom experiments come here (uncomment and adjust the next line)
-    # REGISTRY.append(
-    #     Experiment(
-    #         "MyExp",
-    #         "/myexp/",
-    #         myexp.myexp_bp,
-    #         myexp.apply_config_to
-    #     )
-    # )
+    REGISTRY.append(
+        Experiment(
+            "Descrimage",
+            "/descrimage/",
+            descrimage.descrimage_bp,
+            descrimage.apply_config_to
+        )
+    )
 
     for experiment in REGISTRY:
         experiment.register(app)
