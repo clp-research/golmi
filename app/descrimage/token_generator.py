eac73537215e3f2bc0ff8c281d74d3683de531e5
seba <seba.g90@gmail.com>
2022-03-17 10:27:36 +0100

    token generator will make sure the token is not already taken

--- app/descrimage/token_generator.py
+++ app/descrimage/token_generator.py
@@ -1,21 +1,49 @@
 import secrets
 import string
+import time
+
+import socketio
+
+from app.app import AUTH
 
 
-def generate_random_token():
-    alphabet = string.ascii_letters + string.digits
-    token = "".join(secrets.choice(alphabet) for _ in range(7))
-    return token
+class Generator:
+    def __init__(self, address, auth):
+        self.socket = socketio.Client()
+        self.address = address
+        self.auth = auth
 
+    def callbacks(self):
+        @self.socket.on("room_list")
+        def get_rooms(data):
+            self.taken_rooms = data
 
-def main():
-    token = generate_random_token()
-    tokens = {"receiver": f"{token}-1", "giver": f"{token}-2"}
+    def run(self):
+        self.callbacks()
+        self.socket.connect(self.address, auth={"password": self.auth})
+        self.socket.emit("list_rooms")
+        time.sleep(1)
+        self.socket.disconnect()
 
-    print("Tokens [instruction receiver == admin]:")
-    for key, value in tokens.items():
-        print(f"Instruction {key}:\t{value}")
+    def generate_random_token(self):
+        alphabet = string.ascii_letters + string.digits
+        token = "".join(secrets.choice(alphabet) for _ in range(7))
+        return token
+
+    def generate(self):
+        """
+        generate a unique token
+        """
+        token = self.generate_random_token()
+        while token in self.taken_rooms:
+            token = self.generate_random_token()
+        return {"receiver": f"{token}-1", "giver": f"{token}-2"}
 
 
 if __name__ == "__main__":
-    main()
+    generator = Generator("http://localhost:5000", AUTH)
+    generator.run()
+    tokens = generator.generate()
+    print("Tokens [instruction receiver == admin]:")
+    for key, value in tokens.items():
+        print(f"Instruction {key}:\t{value}")
