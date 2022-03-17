d7cd0b3039cc286b84eb98e3a1fc3f9610f36ee8
seba <seba.g90@gmail.com>
2022-03-17 09:34:41 +0100

    PEP8 (black)

--- app/descrimage/token_generator.py
+++ app/descrimage/token_generator.py
@@ -1,22 +1,21 @@
 import secrets
 import string
 
+
 def generate_random_token():
     alphabet = string.ascii_letters + string.digits
-    token = ''.join(secrets.choice(alphabet) for _ in range(7))
+    token = "".join(secrets.choice(alphabet) for _ in range(7))
     return token
+
 
 def main():
     token = generate_random_token()
-    tokens = {
-        "receiver": f"{token}-1",
-        "giver": f"{token}-2"
-    }
-    
+    tokens = {"receiver": f"{token}-1", "giver": f"{token}-2"}
+
     print("Tokens [instruction receiver == admin]:")
     for key, value in tokens.items():
         print(f"Instruction {key}:\t{value}")
-    
+
 
 if __name__ == "__main__":
     main()
