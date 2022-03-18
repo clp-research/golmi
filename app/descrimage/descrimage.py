46b83019ea5c846fdb049f9fa4193e2f747562d6
seba <seba.g90@gmail.com>
2022-03-18 21:14:43 +0100

    a description can be sent with socketio

--- app/descrimage/descrimage.py
+++ app/descrimage/descrimage.py
@@ -4,6 +4,7 @@ from flask_cors import cross_origin
 import json
 import os
 from datetime import datetime
+from app.app import socketio
 
 
 def apply_config_to(app):
@@ -27,46 +28,41 @@ descrimage_bp = Blueprint('descrimage_bp', __name__,
 
 
 #@cross_origin
-@descrimage_bp.route("/", methods=["GET"])
+@descrimage_bp.route("/", methods=["GET", "POST"])
 def homepage():
     """
     Interactive interface.
     """
-    return render_template("home.html")
+    if "token" in request.form:
+        token = request.form['token']
+        token, role = token.split("-")
 
-@cross_origin
-@descrimage_bp.route('/', methods=['POST'])
-def my_form_post():
-    token = request.form['token']
-    token, role = token.split("-")
-
-    if role == "1":
-        return receiver(token)
-    elif role == "2":
-        return giver(token)
+        if role == "1":
+            return receiver(token)
+        elif role == "2":
+            return giver(token)
+        else:
+            return "INVALID TOKEN"
     else:
-        return "INVALID TOKEN"
+        return render_template("home.html")
 
-#@descrimage_bp.route('/receiver', methods=['GET'])
+
+# @descrimage_bp.route('/', methods=['POST', "GET"])
+# def my_form_post():
+    
+    # else:
+    #     description = request.form["description"]
+    #     print(description)
+
+@descrimage_bp.route('/receiver', methods=['GET'])
 def receiver(token):
     return render_template("receiver.html", token=token)
 
-#@descrimage_bp.route('/giver', methods=['GET'])
+@descrimage_bp.route('/giver', methods=['GET'])
 def giver(token):
     return render_template("giver.html", token=token)
 
-
 # TODO: add socketios
-# @socketio.on("dynamatt_mouseclick")
-# def on_mouseclick(event):
-#     # looks like we need a "mouse"-gripper b.c. everything expects a gripper instance
-#     model = room_manager.get_models_of_client(request.sid)[0]
-#     x, y = translate(event["offset_x"], event["offset_y"], event["block_size"])
-
-#     if "mouse" in model.state.grippers:
-#         model.remove_gr("mouse")
-#         for obj in model.state.objs.values():
-#             obj.gripped = False
-
-#     model.add_gr("mouse", x, y)
-#     model.grip("mouse")
+@socketio.on("descrimage_description")
+def on_mouseclick(description):
+    print(description)
