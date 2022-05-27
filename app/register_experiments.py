61e50e9b7f4867e499a4b51d66d193a822c366b2
Philipp Sadler <philipp.sadler@gmail.com>
2022-05-27 13:45:37 +0200

    Remove the pentomino demo experiment

--- app/register_experiments.py
+++ app/register_experiments.py
@@ -1,21 +1,11 @@
 from app import REGISTRY, Experiment
 from app.welcome import welcome
-from app.pentomino import pentomino
 from app.descrimage import descrimage
 
 def register_app(app):
     # The welcome page (will automatically list
     # all experiments appended to REGISTRY)
     REGISTRY.append(Experiment("Welcome", "/", welcome.welcome_bp))
-    # The demo experiment (you can comment this out, if you dont need it)
-    REGISTRY.append(
-        Experiment(
-            "Pentomino",
-            "/pentomino/",
-            pentomino.pentomino_bp,
-            pentomino.apply_config_to
-        )
-    )
     # Your custom experiments come here (uncomment and adjust the next line)
     REGISTRY.append(
         Experiment(
